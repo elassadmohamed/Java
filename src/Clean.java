c002cda96b0def3933f65f2d5b2aa912d60aa252
elassadmohamed <elassad.mohamed@gmail.com>
2017-12-26 12:33:26 +0000

    test static

--- src/Clean.java
+++ src/Clean.java
@@ -1,4 +1,16 @@
 
 public class Clean {
-
+	static int var;
+	static {
+		int h=6;
+       var=1;
+        System.out.println("dfdfdf"+var);
+        }
+		
+	
+	public static void main(String a []){
+		Clean c=new Clean();
+		
+		System.out.println(Clean.var);
+	}
 }
