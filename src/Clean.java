637eb88288edd44e75b5c91029b3498e5d7afb30
elassadmohamed <elassad.mohamed@gmail.com>
2017-12-26 14:34:42 +0000

    Cleaner

--- src/Clean.java
+++ src/Clean.java
@@ -3,27 +3,38 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Clean {
-	
-	private static Path desktop=Paths.get("C:","Users","mei","Desktop");
-	
-	
-	public static void main(String a []){
-		
+	private static Path desktop = Paths.get("C:", "Users", "mei", "Desktop");
+	private static List<String> target;
+	static {
+		target = new ArrayList<String>();
+		target.add("EFFIA");
+		target.add("AFKLM");
+	}
+
+	public static void main(String a[]) {
 		try {
-			DirectoryStream<Path> dirStream=Files.newDirectoryStream(desktop);
-			for(Path file:dirStream){
-					System.out.println(file.getFileName());				
+			DirectoryStream<Path> dirStream = Files.newDirectoryStream(desktop);
+			for (Path f : dirStream) {
+				Path file = desktop.resolve(f);
+				if (!Files.isDirectory(file)) {
+					for (String t : target) {
+						if(file.toString().toUpperCase().contains(t.toUpperCase())){
+							System.out.println(Paths.get("D:",t).resolve(f.getFileName()).toString());
+							Files.move(file,Paths.get("D:",t).resolve(f.getFileName()),StandardCopyOption.REPLACE_EXISTING);
+						}
+
+					}
+				}
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		
+
 	}
 }
