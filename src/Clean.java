897230f2e9b5080453d18a7c5fa49ed66ea85655
elassadmohamed <elassad.mohamed@gmail.com>
2017-12-26 13:49:34 +0000

    V1 Cleaner

--- src/Clean.java
+++ src/Clean.java
@@ -1,12 +1,29 @@
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class Clean {
 	
+	private static Path desktop=Paths.get("C:","Users","mei","Desktop");
 	
 	
 	public static void main(String a []){
-		Clean c=new Clean();
 		
-		System.out.println("From Eclipse 13:35");
+		try {
+			DirectoryStream<Path> dirStream=Files.newDirectoryStream(desktop);
+			for(Path file:dirStream){
+					System.out.println(file.getFileName());				
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
 		
 	}
 }
